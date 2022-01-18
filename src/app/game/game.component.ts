f3eceeddd9bb04893496ce77aa6096913b34d20f
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-01-18 21:33:25 +0100

    dialog-field close function added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -41,7 +41,9 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
       this.game.players.push(name);
+    }
     });
   }
 }
