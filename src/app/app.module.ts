bfec3544bcba57277b6108e49e3ea9960c507d49
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-01-21 16:56:56 +0100

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,17 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
 import {MatCardModule} from '@angular/material/card';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 
 @NgModule({
@@ -34,9 +45,21 @@ import {MatCardModule} from '@angular/material/card';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [ AppComponent ]
   
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-multiplayer',
+    appId: '1:118710115951:web:8ea5c147e3814f9b96779a',
+    storageBucket: 'ring-of-fire-multiplayer.appspot.com',
+    apiKey: 'AIzaSyBdh5x4ntP8obGfOFODgAgFAM_D7Sfg5uY',
+    authDomain: 'ring-of-fire-multiplayer.firebaseapp.com',
+    messagingSenderId: '118710115951',
+    measurementId: 'G-WYC0WQP5T4',
+  },
   production: false
 };
 
