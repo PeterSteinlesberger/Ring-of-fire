98ef5b740287eb4a2d6f9ad918512b0de0ac0cea
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-01-24 12:58:27 +0100

    firebase error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { provideMessaging,getMessaging } from '@angular/fire/messaging';
 import { providePerformance,getPerformance } from '@angular/fire/performance';
 import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 
 @NgModule({
@@ -46,6 +47,7 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     MatInputModule,
     MatCardModule,
     FormsModule,
+    AngularFirestoreModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
     provideAuth(() => getAuth()),
